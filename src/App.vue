3b597cacbe28ededcd5a3f2d0b4a0b2ddc31a29b
Yatskiv Kath <iamkate4@gmail.com>
2022-08-08 23:04:39 +0200

    section greetings & section create-todo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,96 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, watch, computed } from "vue";
 
-<template>Hello World</template>
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() == "" || input_category.value == null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date(),
+  });
+};
+
+watch(
+  todos,
+  (newValue) => {
+    localStorage.setItem("todos", newValue);
+  },
+  { deep: true }
+);
+
+watch(name, (newValue) => {
+  localStorage.setItem("name", newValue);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
+
+<template>
+  <section class="greeting">
+    <h2 class="greeting__title">
+      Hello, <input type="text" placeholder="Type Your Name" v-model="name" />
+    </h2>
+  </section>
+
+  <section class="create-todo">
+    <h3 class="create-todo__title">Create a Todo</h3>
+
+    <form @submit.prevent="addTodo">
+      <h4>What's on your todo list?</h4>
+      <input
+        type="text"
+        placeholder="e.g. make a video"
+        v-model="input_content"
+      />
+
+      <h4>Pick a Category</h4>
+      <div class="create-todo__options">
+        <label>
+          <input
+            type="radio"
+            name="category"
+            value="business"
+            v-model="input_category"
+          />
+          <span class="bubble business"></span>
+          <div>Business</div>
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="category"
+            value="business"
+            v-model="input_category"
+          />
+          <span class="bubble personal"></span>
+          <div>Personal</div>
+        </label>
+      </div>
+
+      <input type="submit" value="Add Todo" />
+    </form>
+  </section>
+</template>
 
 <style scoped>
 header {
